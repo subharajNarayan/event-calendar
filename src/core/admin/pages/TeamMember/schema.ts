6fc147d3f9fe90fa4396e6c08e466d0fcfd860a5
Subharaj Chaudhary <subharazzchy68@gmail.com>
2024-03-20 12:57:02 +0545

    update TeamMember email field to unrequired

--- src/core/admin/pages/TeamMember/schema.ts
+++ src/core/admin/pages/TeamMember/schema.ts
@@ -13,10 +13,10 @@ export const TeamValidationSchema = Yup.object().shape({
   username: Yup.string().required('Username is required'),
   contactnumber: Yup.string().matches(/^\d+$/, 'Enter numbers only'),
   password: Yup.string().required('Password is required'),
-  email: Yup.string()
-    .matches(
-      /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-      'Invalid email format'
-    )
-    .required('Email is required'),
+  // email: Yup.string()
+  //   .matches(
+  //     /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  //     'Invalid email format'
+  //   )
+  //   .required('Email is required'),
 });
